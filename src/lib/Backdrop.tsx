9bda622cf78b467fad2679729459517a79936e32
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-05-15 16:37:49 +0900

    transition

--- src/lib/Backdrop.tsx
+++ src/lib/Backdrop.tsx
@@ -1,9 +1,14 @@
 import { Component, useContext } from "solid-js";
+import { Transition } from "solid-transition-group";
 import { ModalContext } from "./ModalContext";
 
 export const Backdrop: Component = () => {
   const { stack } = useContext(ModalContext);
   const showing = () => stack().length > 0;
 
-  return <>{showing() ? <div class="zsm-backdrop"></div> : null}</>;
+  return (
+    <Transition name="zsm-t">
+      {showing() ? <div class="zsm-backdrop"></div> : null}
+    </Transition>
+  );
 };
--- src/lib/Renderer.tsx
+++ src/lib/Renderer.tsx
@@ -1,9 +1,14 @@
 import { Component, useContext } from "solid-js";
+import { Transition } from "solid-transition-group";
 import { ModalContext } from "./ModalContext";
 
 export const Renderer: Component = () => {
   const { stack } = useContext(ModalContext);
   const top = () => stack().at(-1);
 
-  return <>{((Component) => Component && <Component />)(top())}</>;
+  return (
+    <Transition name="zsm-t">
+      {((Component) => Component && <Component />)(top())}
+    </Transition>
+  );
 };
